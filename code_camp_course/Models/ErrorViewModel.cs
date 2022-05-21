efce13d5b2caa169cdfc512baf026c17b55040d8
Mikhail Pelagein <retvain@gmail.com>
2022-05-21 17:58:55 +0500

    init api

--- code_camp_course/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace code_camp_course.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
